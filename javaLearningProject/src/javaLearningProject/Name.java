1742991813e17cef3b27e50b0d7848f9e2507bd8
Akshay <singhakhay582@gmail.com>
2023-06-29 14:23:04 +0530

    Intial Commit

---
+++ javaLearningProject/src/javaLearningProject/Name.java
@@ -0,0 +1,10 @@
+package javaLearningProject;
+
+public class Name {
+
+	public static void main(String[] args) {
+
+		System.out.println("My name is Akshay");
+	}
+
+}
---
+++ javaLearningProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ankita
+ *
+ */
+module javaLearningProject {
+}
